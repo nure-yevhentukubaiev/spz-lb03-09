c110266696932bab53d75534aa32dd1e21f29bee
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-18 23:52:35 +0300

    spz-lb03-09-task02: part01 done.

--- spz-lb03-09-task02/main.cpp
+++ spz-lb03-09-task02/main.cpp
@@ -1,24 +1,47 @@
 #include <ntddk.h>
-VOID SampleUnload(_In_ PDRIVER_OBJECT DriverObject)
+
+VOID
+OnDrvUnload(
+	_In_ PDRIVER_OBJECT DriverObject
+);
+
+VOID
+OnDrvUnload(
+	_In_ PDRIVER_OBJECT DriverObject
+)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 
 	KdPrint(("Func: %s\n", __FUNCTION__));
 	return;
 }
+
 extern "C"
 NTSTATUS
 DriverEntry(
-	_In_ PDRIVER_OBJECT DriverObject,
-	_In_ PUNICODE_STRING RegistryPath
+	_In_ PDRIVER_OBJECT pDriverObject,
+	_In_ PUNICODE_STRING puniszRegistryPath
 )
 {
 	NTSTATUS nt = STATUS_SUCCESS;
+	PDEVICE_OBJECT pDeviceObject = NULL;
+	UNICODE_STRING uniszDevice;
+	RtlInitUnicodeString(&uniszDevice, L"\\Devices\\Simple2");
 
-	UNREFERENCED_PARAMETER(RegistryPath);
-	DriverObject->DriverUnload = SampleUnload;
-
+	UNREFERENCED_PARAMETER(puniszRegistryPath);
+	
 	KdPrint(("Func: %s\n", __FUNCTION__));
 
+	nt = IoCreateDevice(
+		pDriverObject,
+		0, &uniszDevice, 50000, 0, FALSE, &pDeviceObject
+	);
+	if (nt != STATUS_SUCCESS) {
+		goto fail;
+	}
+
+	pDriverObject->DriverUnload = OnDrvUnload;
+
+	fail:
 	return nt;
 }
