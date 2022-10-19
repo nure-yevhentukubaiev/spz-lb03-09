b160fe8578d59a3b58211d0575633e6f00206d11
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-19 11:42:07 +0300

    spz-lb03-09-task02: part02 done.

--- spz-lb03-09-task02/main.cpp
+++ spz-lb03-09-task02/main.cpp
@@ -5,14 +5,58 @@ OnDrvUnload(
 	_In_ PDRIVER_OBJECT DriverObject
 );
 
+NTSTATUS
+OnCreate(
+	PDEVICE_OBJECT pDeviceObject,
+	PIRP pIrp
+);
+
+NTSTATUS
+OnClose(
+	PDEVICE_OBJECT pDeviceObject,
+	PIRP pIrp
+);
+
+NTSTATUS
+OnCreate(
+	PDEVICE_OBJECT pDeviceObject,
+	PIRP pIrp
+)
+{
+	UNREFERENCED_PARAMETER(pDeviceObject);
+	KdPrint(("Func: %s\n", __FUNCTION__));
+
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	return pIrp->IoStatus.Status;
+}
+
+NTSTATUS
+OnClose(
+	PDEVICE_OBJECT pDeviceObject,
+	PIRP pIrp
+)
+{
+	UNREFERENCED_PARAMETER(pDeviceObject);
+	KdPrint(("Func: %s\n", __FUNCTION__));
+
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	
+	return pIrp->IoStatus.Status;
+}
+
 VOID
 OnDrvUnload(
 	_In_ PDRIVER_OBJECT DriverObject
 )
 {
 	UNREFERENCED_PARAMETER(DriverObject);
-
 	KdPrint(("Func: %s\n", __FUNCTION__));
+
 	return;
 }
 
@@ -36,11 +80,13 @@ DriverEntry(
 		pDriverObject,
 		0, &uniszDevice, 50000, 0, FALSE, &pDeviceObject
 	);
-	if (nt != STATUS_SUCCESS) {
+	if (!NT_SUCCESS(nt)) {
 		goto fail;
 	}
 
 	pDriverObject->DriverUnload = OnDrvUnload;
+	pDriverObject->MajorFunction[IRP_MJ_CREATE] = OnCreate;
+	pDriverObject->MajorFunction[IRP_MJ_CLOSE] = OnClose;
 
 	fail:
 	KdPrint(("Func: %s returns %lX\n", __FUNCTION__, nt));
