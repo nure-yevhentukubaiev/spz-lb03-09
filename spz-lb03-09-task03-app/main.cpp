d175a75039c974fb6381151d7d323e2da4ec5386
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-20 15:50:40 +0300

    spz-lb03-09-task03: add testing app.

---
+++ spz-lb03-09-task03-app/main.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	DWORD bErr = 0;
+	HANDLE hFile = CreateFile(
+		_T("\\\\.\\Simple3Link"), GENERIC_READ,
+		FILE_SHARE_READ, NULL, OPEN_EXISTING,
+		NULL,
+		NULL
+	);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		bErr = GetLastError();
+		_tprintf_s(
+			_T("Function CreateFile failed, error: %08lX\n"), bErr
+		);
+		goto fail;
+	} else {
+		_tprintf_s(
+			_T("Function CreateFile succeded, handle: %pX\n"), hFile
+		);
+	}
+
+	if (FAILED(CloseHandle(hFile))) {
+		bErr = GetLastError();
+		_tprintf_s(
+			_T("Function CloseHandle failed, error: %08lX\n"), bErr
+		);
+	} else {
+		_tprintf_s(
+			_T("Function CloseHandle succeded\n")
+		);
+	}
+	
+
+	fail:
+	return bErr;
+}
